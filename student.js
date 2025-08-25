81638f89811333507c10b7fca268fcda7a6fb081
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-08-25 14:38:25 -0700

    Create student.js

---
+++ student.js
@@ -0,0 +1,12 @@
+// Variables created at the top of JavaScript file (not in a function)
+var thename = "Brandon Chang";
+var my_message = "cs190 Javascript Lab 2";
+var id = 878;
+
+// Function to display all variable information in an alert box
+function displayinfo871() {
+    var message = "Name: " + thename + "\n" + 
+                  "Message: " + my_message + "\n" + 
+                  "ID: " + id;
+    alert(message);
+}
