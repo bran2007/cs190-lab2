34a17877f7591e0b11eea0ca804aeb3e3d501f52
Brandon Chang <62526752+bran2007@users.noreply.github.com>
2025-09-19 18:30:28 -0700

    Update student2.js

--- student2.js
+++ student2.js
@@ -3,49 +3,63 @@ var companyname4 = "Valencia Book House";
 var address4 = "962 Main St. Valencia, CA 91344";
 var phonenumber5 = "(555) 638-1234";
 
-// Product objects
-var product1 = { name:"HP 4426", id:"4426", desc:"Newest and Best Laptop from HP Computer" };
-var product2 = { name:"Apple 88123 iPad", id:"88123", desc:"Apple Tablet Computer" };
-var product3 = { name:"Dell Dimension 2400", id:"2400X", desc:"A fast 2.4 ghz computer" };
+// Global URL bases
+var imgurl739 = 'https://www.college1.com/images/books/';  // book images
+var cardimgurl550 = 'https://www.college1.com/images/cards/gbCard';  // blackjack cards
+
+// Product objects (Books)
+var product1 = { name:"The Cosmos Explained", id:"book001", desc:"A complete guide to understanding the universe." };
+var product2 = { name:"Journey Through Space", id:"book002", desc:"Explore the wonders of space travel." };
+var product3 = { name:"Astronomy for Beginners", id:"book003", desc:"Learn the basics of stargazing and planets." };
 
 // Header function
 function getHeader() {
-  var headerHTML = "<h1 style='color:#004488; font-size:2.5em;'>" + companyname4 + "</h1>";
-  return headerHTML;
+  return "<h1 style='color:#004488; font-size:2.5em;'>" + companyname4 + "</h1>";
 }
 
 // Footer function
 function getFooter(name, address, phone) {
-  var footerHTML = "<p style='color:#666;'>" 
-                 + name + " | " + address + " | " + phone
-                 + " | " + navigator.appName
-                 + " " + navigator.appVersion
-                 + " | " + navigator.platform
-                 + "</p>";
-  return footerHTML;
+  return "<p style='color:#666;'>" 
+       + name + " | " + address + " | " + phone
+       + " | " + navigator.appName
+       + " " + navigator.appVersion
+       + " | " + navigator.platform
+       + "</p>";
 }
 
-// Make menu with product buttons
+// Make menu with product buttons + Deal Cards
 function makeMenu2() {
   var menuHTML = "Menu: ";
   menuHTML += "<button onclick='execButton382(product1)'>Product #1</button>";
   menuHTML += "<button onclick='execButton382(product2)'>Product #2</button>";
   menuHTML += "<button onclick='execButton382(product3)'>Product #3</button>";
+  menuHTML += "<button onclick='dealCards649()'>Deal Cards</button>";
   return menuHTML;
 }
 
-// Make main section with dynamic product
+// Make main section with dynamic product and image
 function makeMain9(myproduct) {
-  var mainHTML = "My Product:<br>" +
-                 "Product Name: " + myproduct.name + "<br>" +
-                 "Product ID: " + myproduct.id + "<br>" +
-                 "Product Description: " + myproduct.desc + "<br>" +
-                 "Shopping Cart Link Bar <br>" +
-                 "Product Image: <br>";
-  return mainHTML;
+  var produrl = imgurl739 + myproduct.id + '.gif';
+  return "My Product:<br>" +
+         "Product Name: " + myproduct.name + "<br>" +
+         "Product ID: " + myproduct.id + "<br>" +
+         "Product Description: " + myproduct.desc + "<br>" +
+         "Shopping Cart Link Bar <br>" +
+         "Product Image: <br><img src='" + produrl + "' alt='" + myproduct.name + "'>";
 }
 
 // Execute button click to change product
 function execButton382(myproduct) {
   document.getElementById("main895").innerHTML = makeMain9(myproduct);
+}
+
+// Deal cards function
+function dealCards649() {
+  var cardHTML = "";
+  for (var i = 0; i < 5; i++) {
+    var cardnumber = (i < 2) ? i : 52;  // first 2 face up, last 3 face down
+    var cardurl = cardimgurl550 + cardnumber + '.gif';
+    cardHTML += "<img id='card" + i + "' src='" + cardurl + "' alt='Card " + i + "'> ";
+  }
+  document.getElementById("main895").innerHTML = cardHTML;
 }
